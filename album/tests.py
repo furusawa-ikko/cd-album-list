094abd5370f18a84b4b094534b77141503b92621
furusawa-ikko <85667118+furusawa-ikko@users.noreply.github.com>
2023-12-06 22:53:49 +0900

    Update tests.py

--- album/tests.py
+++ album/tests.py
@@ -1,22 +1,10 @@
 from django.test import TestCase
 
+from album.apps import AlbumConfig
+from album.models import Artists
+
 # Create your tests here.
-# views.py または任意の適切な場所に以下のコードを追加します。
+for album in AlbumConfig.Albums:
+    for track in Artists.Tracks:
+        print(track.name)
 
-from django.shortcuts import render
-
-
-def your_view_function(request):
-    # アルバムをすべて取得
-    albums = Albums.objects.all()
-
-    # 各アルバムに関連するトラックを取得するためのループ
-    for album in albums:
-        # ここでN回の追加のSELECTクエリが発生 (N+1問題)
-        tracks = album.tracks.all()
-        
-        # 取得したトラックを処理する（例えば、表示するか何かを行う）
-        for track in tracks:
-            print(track.name)
-
-    return render(request, 'your_template.html', {'albums': albums})
