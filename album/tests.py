861b267c7981d1fb79c5385d2da66653036a459a
furusawa-ikko <85667118+furusawa-ikko@users.noreply.github.com>
2023-12-07 13:33:14 +0900

    Update tests.py

--- album/tests.py
+++ album/tests.py
@@ -1,11 +1,2 @@
 from django.test import TestCase
-
-from album.models import Albums
-
-
-
 # Create your tests here.
-for album in Albums.objects.all():
-    for track in album.Tracks:
-        print(track.name)
-
